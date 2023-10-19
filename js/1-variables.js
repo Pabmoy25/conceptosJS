6e38b036ef002198816da0b6312684f0dd86c484
Pablo Moyano <pabmoy1725@gmail.com>
2023-10-19 17:56:42 -0300

    agregamos variables, mensajes por consola y pantalla

--- js/1-variables.js
+++ js/1-variables.js
@@ -7,3 +7,34 @@ lineas */
 // Declarar una variable (Var - let - const)
 
 let anioActual = 2023;
+const url = "https://universe.rollingcodeschool.com";
+
+// Mostrar mensajes
+
+console.log("Este es un mensaje por consola");
+console.log (anioActual);
+console.log (url)
+console.log ("Año: "+ (anioActual + 2));
+
+// Modificar el contenido de una variable
+anioActual = 2024;
+console.log (anioActual);
+
+// url = "otro valor";
+console.log (url);
+
+document.write ("<h1>Prueba de <br> titulo</h1>");
+document.write ("Año: "+ anioActual + "<br>");
+document.write ("Año: "+ anioActual);
+
+// ventanas emergentes
+// alert ("Esta es una prueba de ventana emergente");
+
+// graba una variable, siempre texto
+let color = prompt ("Ingrese un color");
+console.log (color);
+
+
+
+
+
